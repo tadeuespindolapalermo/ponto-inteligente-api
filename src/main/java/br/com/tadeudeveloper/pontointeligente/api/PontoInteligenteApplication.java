404b0a71e91ca4a7f59da6e9be8dceb3f4fe3f27
Tadeu Espíndola Palermo <tadeupalermoti@gmail.com>
2018-02-02 15:13:16 -0200

    Commit inicial dos arquivos do projeto

---
+++ src/main/java/br/com/tadeudeveloper/pontointeligente/api/PontoInteligenteApplication.java
@@ -0,0 +1,13 @@
+package br.com.tadeudeveloper.pontointeligente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Start do Spring Boot
+@SpringBootApplication
+public class PontoInteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoInteligenteApplication.class, args);
+	}
+}
