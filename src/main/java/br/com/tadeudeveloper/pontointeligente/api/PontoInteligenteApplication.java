d3e99b1d638e7cb362d2834cf433f83ff997ddf7
Tadeu Espíndola Palermo <tadeupalermoti@gmail.com>
2018-02-02 16:54:10 -0200

    Documentação da classe

--- src/main/java/br/com/tadeudeveloper/pontointeligente/api/PontoInteligenteApplication.java
+++ src/main/java/br/com/tadeudeveloper/pontointeligente/api/PontoInteligenteApplication.java
@@ -3,7 +3,9 @@ package br.com.tadeudeveloper.pontointeligente.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// Start do Spring Boot
+/** 
+ * Start do Spring Boot 
+ */
 @SpringBootApplication
 public class PontoInteligenteApplication {
 
