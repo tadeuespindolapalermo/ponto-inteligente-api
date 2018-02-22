6bb6062be1924f4175e6d06fa89b03271bc51852
Tadeu Espíndola Palermo <tadeupalermoti@gmail.com>
2018-02-22 00:11:20 -0300

    Adicionado EhCache para aumentar a performance da aplicação

--- src/main/java/br/com/tadeudeveloper/pontointeligente/api/PontoInteligenteApplication.java
+++ src/main/java/br/com/tadeudeveloper/pontointeligente/api/PontoInteligenteApplication.java
@@ -2,11 +2,13 @@ package br.com.tadeudeveloper.pontointeligente.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 /** 
  * Start do Spring Boot 
  */
 @SpringBootApplication
+@EnableCaching
 public class PontoInteligenteApplication {
 
 	public static void main(String[] args) {
